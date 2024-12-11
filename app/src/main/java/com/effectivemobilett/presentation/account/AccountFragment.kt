7e5cd9252bb4d2f13e241a98922cca1e8de258d5
Ibrahim Dekkush <jashibrash@mail.ru>
2024-12-11 06:16:42 +0300

    [AccountFragment\SaveFragment] Added account and save fragments

---
+++ app/src/main/java/com/effectivemobilett/presentation/account/AccountFragment.kt
@@ -0,0 +1,11 @@
+package com.effectivemobilett.presentation.account
+
+import androidx.fragment.app.Fragment
+import com.example.effectivemobilett.databinding.FragmentAccountBinding
+
+class AccountFragment : Fragment() {
+
+    private var _binding: FragmentAccountBinding? = null
+
+    private val binding get() = _binding!!
+}
---
+++ app/src/main/java/com/effectivemobilett/presentation/save/SaveFragment.kt
@@ -0,0 +1,12 @@
+package com.effectivemobilett.presentation.save
+
+import androidx.fragment.app.Fragment
+import com.example.effectivemobilett.databinding.FragmentSaveBinding
+
+class SaveFragment: Fragment() {
+
+    private var _binding: FragmentSaveBinding? = null
+
+    private val binding get() = _binding!!
+
+}
